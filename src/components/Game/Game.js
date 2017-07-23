4e2bf97633195a15646277d3222d45a06e8b721c
Alex Rapley <rapley3@gmail.com>
2017-07-23 16:43:22 +1000

    Add player tracking and move viewport accordingly

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -12,31 +12,49 @@ class Game extends Component {
     super();
 
     this.state = {
+      viewport: {
+        x: 0,
+        y: 0
+      },
       player: {
         x: 135,
-        y: 30
+        y: 510
       }
     }
 
     this.loop = this.loop.bind(this);
   }
 
-  componentDidMount() {
+  componentWillMount() {
     setInterval(this.loop, 16);
+    this.loop();
   }
 
   loop() {
+
+    //move player x-many pixels, based on key pressed
     let xDelta = 0;
     if (this.props.keyMap.ArrowRight) {
-      xDelta = 1;
+      xDelta = 3;
     } else if (this.props.keyMap.ArrowLeft) {
-      xDelta = -1;
+      xDelta = -3;
     }
+    //create new player state
+    const player = {
+      x: this.state.player.x + xDelta,
+      y: this.state.player.y
+    };
+    //create new viewport state
+    const viewport = {
+      //since we cant move the viewport,
+      //we move the game into it.
+      x: window.innerWidth/2 - player.x,
+      y: window.innerHeight/2 - player.y
+    };
+
     this.setState({
-      player: {
-        x: this.state.player.x + xDelta,
-        y: this.state.player.y
-      }
+      viewport,
+      player
     });
   }
 
@@ -44,11 +62,11 @@ class Game extends Component {
 
   render() {
     return (
-      <div>
+      <game className="entity" style={{ top: this.state.viewport.y, left: this.state.viewport.x }}>
         <Background />
         <Ground />
         <Player pos={this.state.player} />
-      </div>
+      </game>
     )
   }
 }
